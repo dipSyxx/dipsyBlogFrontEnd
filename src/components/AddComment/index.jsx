ed203f4cc807a7973077d3ce2a371eb1268dea0f
Yura <linetsky.yura@gmail.com>
2023-03-04 23:06:57 +0200

    added comment icon

--- src/components/AddComment/index.jsx
+++ src/components/AddComment/index.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "./AddComment.module.scss";
 
 import TextField from "@mui/material/TextField";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
+import { SelectIsAuth } from "../../redux/slices/auth";
+import { useSelector } from "react-redux";
+import axios from "../../axios";
 
 export const Index = () => {
+  const isAuth = useSelector(SelectIsAuth);
+  const [avatarUrl, setAvatarUrl] = useState("");
+
+  useEffect(() => {
+    axios.get("/auth/me").then(({ data }) => {
+      setAvatarUrl(data.avatarUrl);
+    });
+  }, []);
   return (
     <>
       <div className={styles.root}>
-        <Avatar
-          classes={{ root: styles.avatar }}
-          src="https://mui.com/static/images/avatar/5.jpg"
-        />
+        <Avatar classes={{ root: styles.avatar }} src={avatarUrl} />
         <div className={styles.form}>
           <TextField
             label="Написати коментар"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Grid from "@mui/material/Grid";
@@ -12,6 +12,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts, fetchTags } from "../redux/slices/posts";
 
 export const Home = () => {
+  const [value, setValue] = useState(0);
   const dispatch = useDispatch();
   const userData = useSelector((state) => state.auth.data);
   const { posts, tags } = useSelector((state) => state.posts);
@@ -26,37 +27,73 @@ export const Home = () => {
 
   console.log(posts);
 
+  const handleSwitchPopularity = () => {
+    setValue(1);
+  };
+  const handleSwitchNew = () => {
+    setValue(0);
+  };
+
   return (
     <>
       <Tabs
         style={{ marginBottom: 15 }}
-        value={0}
+        value={value}
         aria-label="basic tabs example"
       >
-        <Tab label="Нові" />
-        <Tab label="Популярні" />
+        <Tab onClick={handleSwitchNew} label="Нові" />
+        <Tab onClick={handleSwitchPopularity} label="Популярні" />
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
-            isPostsLoading ? (
-              <Post key={index} isLoading={true} />
-            ) : (
-              <Post
-                _id={obj._id}
-                title={obj.title}
-                imageUrl={
-                  obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ""
-                }
-                user={obj.user}
-                createdAt={obj.createdAt}
-                viewsCount={obj.viewsCount}
-                commentsCount={3}
-                tags={obj.tags}
-                isEditable={userData?._id === "63f4a6d3f31dc420832e52d8"}
-              />
-            )
-          )}
+          {value === 0
+            ? (isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+                isPostsLoading ? (
+                  <Post key={index} isLoading={true} />
+                ) : (
+                  <Post
+                    _id={obj._id}
+                    title={obj.title}
+                    imageUrl={
+                      obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ""
+                    }
+                    user={obj.user}
+                    createdAt={obj.createdAt}
+                    viewsCount={obj.viewsCount}
+                    commentsCount={3}
+                    tags={obj.tags}
+                    isEditable={userData?._id === "63f4a6d3f31dc420832e52d8"}
+                  />
+                )
+              )
+            : ""}
+
+          {value === 1
+            ? posts.items
+                .filter((post) => post.viewsCount > 5)
+                .sort((a, b) => a.viewsCount - b.viewsCount)
+                .map((post, index) =>
+                  isPostsLoading ? (
+                    <Post key={index} isLoading={true} />
+                  ) : (
+                    <Post
+                      _id={post._id}
+                      title={post.title}
+                      imageUrl={
+                        post.imageUrl
+                          ? `http://localhost:4444${post.imageUrl}`
+                          : ""
+                      }
+                      user={post.user}
+                      createdAt={post.createdAt}
+                      viewsCount={post.viewsCount}
+                      commentsCount={3}
+                      tags={post.tags}
+                      isEditable={userData?._id === "63f4a6d3f31dc420832e52d8"}
+                    />
+                  )
+                )
+            : ""}
         </Grid>
         <Grid xs={4} item>
           <TagsBlock items={tags.items} isLoading={isTagsLoading} />
